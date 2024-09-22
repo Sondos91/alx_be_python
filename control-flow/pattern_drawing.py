c38cd807bc5d68a25ef377ae9b02e7b113ac036a
Sondos91 <sondossfwat@gmail.com>
2024-09-22 14:49:37 +0300

    correction

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,16 +1,8 @@
-def draw_pattern(size):
-    row = 0
-    while row < size:
-        for _ in range(size):
-            print("*", end="")
-        print()
-        row += 1
+size = int(input("Enter the size of the pattern: "))
 
-def main():
-    size = int(input("Enter the size of the pattern: "))
-    while size <= 0:
-        size = int(input("Please enter a positive integer: "))
-    draw_pattern(size)
-
-if __name__ == "__main__":
-    main()
+row = 0
+while row < size:
+    for _ in range(size):
+        print("*", end="")
+    print()
+    row += 1
